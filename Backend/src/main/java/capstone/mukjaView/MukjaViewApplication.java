e9e1c89f1a52cff468e07d24128f6f77714dd62e
chiwon <dh5277@naver.com>
2024-04-15 14:20:10 +0900

    spring init

---
+++ Backend/src/main/java/capstone/mukjaView/MukjaViewApplication.java
@@ -0,0 +1,14 @@
+package capstone.mukjaView;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MukjaViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MukjaViewApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/capstone/mukjaView/MukjaViewApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.mukjaView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MukjaViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
