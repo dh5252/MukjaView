b9d68b1c1d2294cc9026a84963ebafa0be28d31f
chiwon <dh5277@naver.com>
2024-04-17 14:55:11 +0900

    action test2

--- Backend/src/main/java/capstone/mukjaView/MukjaViewApplication.java
+++ Backend/src/main/java/capstone/mukjaView/MukjaViewApplication.java
@@ -4,7 +4,7 @@ package capstone.mukjaView;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//github action test
+//github action test2
 @SpringBootApplication
 public class MukjaViewApplication {
 	public static void main(String[] args) {
